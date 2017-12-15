60908cb616916959b9d360f2f51de0fa85331266
gustavox13 <gurodrigues1300@gmail.com>
2017-12-15 10:36:40 -0200

    add star count

---
+++ Assets/Scripts/Game/Star.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour {
+
+    [SerializeField]
+    private StarCount starCountScript;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == Constants.TagName.PLAYER)
+        {
+            AddStar();
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void AddStar()
+    {
+        starCountScript.NumberStars++;
+    }
+
+}
---
+++ Assets/Scripts/Hud/StarCount.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCount : MonoBehaviour {
+
+    public int NumberStars
+    {
+        get { return numberStars; }
+        set { numberStars = value; }
+    }
+    private int numberStars;
+
+    [SerializeField]
+    private Canvas WinScreen;
+
+    [SerializeField]
+    private GameObject[] stars = new GameObject[3];
+
+    private int numberOfStar = 3;
+
+    private void Start()
+    {
+        numberStars = 0; 
+
+        for (int i = 0; i < numberOfStar; i++)
+        {
+            stars[i].SetActive(false);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (WinScreen.isActiveAndEnabled)
+        {
+            for (int i = 0; i < numberStars; i++)
+            {
+                stars[i].SetActive(true);
+            }
+        }
+    }
+
+
+}
