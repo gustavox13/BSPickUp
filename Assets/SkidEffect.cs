d78463d1dbd062a0ab3fba40ab44bd20c7345d45
gustavox13 <gurodrigues1300@gmail.com>
2018-02-08 13:51:01 -0200

    update effects

---
+++ Assets/SkidEffect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkidEffect : MonoBehaviour {
+    [SerializeField]
+    private Animator skidAnim;
+    private string objectTagFloor;
+    [SerializeField]
+    private bool isOnTheFloor;
+
+
+    private void Start()
+    {
+        LoadResources();
+    }
+
+    private void LoadResources()
+    {
+        skidAnim = GetComponent<Animator>();
+        objectTagFloor = Constants.TagName.FLOOR;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == objectTagFloor)
+        {
+            isOnTheFloor = true;
+        } 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == objectTagFloor)
+        {
+            isOnTheFloor = false;
+        }
+    }
+
+
+    public void Skid()
+    {
+        if (isOnTheFloor)
+        {
+            skidAnim.SetTrigger("Acelerate");
+        }
+    }
+}
---
+++ Assets/SkidLocal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkidLocal : MonoBehaviour {
+
+    private string objectTagPlayer;
+    private GameObject objPlayer;
+
+    private void Start()
+    {
+        LoadResources();
+    }
+
+    private void LoadResources()
+    {
+        objectTagPlayer = Constants.TagName.PLAYER;
+        objPlayer = GameObject.FindWithTag(objectTagPlayer);
+    }
+
+    private void Update()
+    {
+
+        gameObject.transform.position = new Vector3(objPlayer.transform.position.x,
+                                                    objPlayer.transform.position.y,
+                                                    objPlayer.transform.position.z);
+    }
+}
